e67f69cdee203439c3c98f702c689caf4584fd42
Aziz Zulfikorov <tokzor123@yahoo.com>
2023-07-17 11:45:41 +0500

    made some changes to the app

--- src/app/country-search/country-search.component.ts
+++ src/app/country-search/country-search.component.ts
@@ -1,87 +1,5 @@
 
 
-//country-search.components.ts
-/*
-
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
-
-
-
-interface CountryData {
-  name: any;
-  flags: { [key: string]: string };
-  capital: string;
-  continents: string;
-  population: number;
-  currencies: { [code: string]: { name: string, symbol: string } };
-  currencyName?: string; // Updated property name
-  languages: { [code: string]: string };
-  languageName?: string;
-  notFound?: boolean;
-}
-
-@Component({
-  selector: 'app-country-search',
-  templateUrl: './country-search.component.html',
-  styleUrls: ['./country-search.component.css']
-})
-export class CountrySearchComponent {
-  countryName!: string;
-  countryData: CountryData | null = null;
-  currencyName!: string; // Updated property name
-  languageName!: string;
-  showWarning: boolean = false; // Added flag
-  countries!: any[];
-
-  constructor(private http: HttpClient) { }
-
-  searchCountry() {
-    this.countryData = null; // Clear previous data
-    this.http.get<CountryData[]>(`https://restcountries.com/v3.1/name/${this.countryName}`)
-      .subscribe(
-        data => {
-          if (data.length > 0) {
-            this.countryData = data[0];
-            this.retrieveCurrencyName();
-            this.retrieveLanguageName();
-          } else {
-            this.countryData = { notFound: true } as CountryData;
-          }
-        },
-        error => {
-          this.countryData = { notFound: true } as CountryData;
-        }
-      );
-  }
-
-
-  private retrieveCurrencyName() {
-    const currencyCodes = Object.keys(this.countryData?.currencies || {});
-    if (currencyCodes.length > 0) {
-      const currencies = currencyCodes.map(code => {
-        const currency = this.countryData?.currencies?.[code];
-        return `${currency?.name} (${currency?.symbol})`;
-      });
-      this.currencyName = currencies.join(', ');
-    }
-  }
-
-
-  private retrieveLanguageName() {
-    const languageCodes = Object.keys(this.countryData?.languages || {});
-    if (languageCodes.length > 0) {
-      const languages = languageCodes.map(code => this.countryData?.languages?.[code]);
-      this.languageName = languages.join(', ');
-    }
-  }
-
-
-
-
-}
-*/
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormControl } from '@angular/forms';
@@ -108,7 +26,7 @@ interface CountryData {
   styleUrls: ['./country-search.component.css']
 })
 export class CountrySearchComponent {
-  countryName = new FormControl();
+  countryName = new FormControl('');
   filteredCountries!: Observable<string[]>;
 
 
@@ -129,7 +47,7 @@ export class CountrySearchComponent {
 
     // Fetch the list of countries and populate the array
     this.fetchCountries();
-    console.log(this.filteredCountries)
+
 
   }
 
@@ -162,24 +80,24 @@ export class CountrySearchComponent {
 
 
 
-  private fetchCountries() {
-    axios.get<any[]>('https://restcountries.com/v3.1/all')
-      .then(response => {
-        this.countries = response.data.map(country => country.name.common);
-        console.log(response)
-        console.log(this.countries);
-      })
-      .catch(error => {
-        console.error('Failed to fetch countries:', error);
+  fetchCountries() {
+    this.http.get<any[]>('https://restcountries.com/v3.1/all')
+      .subscribe(countries => {
+        this.countries = countries.map(country => country.name.common).sort();
       });
   }
 
-  private filterCountries(value: string): string[] {
-    const filterValue = value.toLowerCase();
-    //console.log(this.countries);
-    return this.countries.filter((country:string) => country.toLowerCase().includes(filterValue));
 
+  filterCountries(value: string | null): string[] {
+    if (!value) {
+      return this.countries;
+    }
+    const filterValue = value.toLowerCase();
+    return this.countries.filter((country: string) =>
+      country.toLowerCase().includes(filterValue)
+    );
   }
+
 
   private retrieveCurrencyName() {
     const currencyCodes = Object.keys(this.countryData?.currencies || {});
@@ -199,4 +117,12 @@ export class CountrySearchComponent {
       this.languageName = languages.join(', ');
     }
   }
+
+  onKeyUp(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.searchCountry();
+    }
+  }
+
+
 }
